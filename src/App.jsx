b0ce98d9b5934f0d4bdc240fb18641bf4cd14420
Vijay Kumar <139329029+vijayiitp@users.noreply.github.com>
2024-10-20 00:39:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get(`${server}api/v1/user/me`, { withCredentials: true })
+      .get(`${server}/api/v1/user/me`, { withCredentials: true })
       .then(({ data }) => dispatch(userExists(data.user)))
       .catch((err) => dispatch(userNotExists()));
   }, [dispatch]);
